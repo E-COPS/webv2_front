825169a2e1e3d2b739dea2b31f809eacfa647139
ahwe <68504138+bsoiyou@users.noreply.github.com>
2022-12-21 13:52:53 +0900

    NavBar, BottomBar - responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../images/logo-img.png';
+import { FiMenu } from "react-icons/fi";
 import styles from './NavBar.module.css';
 
 function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" className={styles.navStyle}>
-      <Container className='px-0'>
+      <Container className='px-0 align-items-center'>
         {/* logo */}
-        <Navbar.Brand href="/">E-COPS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="/">
+          <img src={logo} className={styles.logoImg} alt='e-cops'/>
+        </Navbar.Brand>
+        {/* menu icon */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className='border-0 mx-2'>
+          <FiMenu className={styles.menuIcon}/>
+        </Navbar.Toggle>
         {/* menu */}
         <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
         <Nav className="ml-auto align-items-center">
