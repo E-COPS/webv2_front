787880e213428ddf2022774f2050b10b75da6a3b
mj <mjk4534@ewhain.net>
2023-02-20 04:29:04 +0900

    [Content] Recruit 수정중

--- src/components/Recruitment_Button.js
+++ src/components/Recruitment_Button.js
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
 import styles from './Recruitment_Button.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Button({text}) {
-  return(
-    <button className={styles.btn}>
-        12기 알림 신청하기
-      <Link to=''>{text}</Link>
-    </button>
+function Button({ text }) {
+  return (
+    // <Link
+    //   to={{ pathname: 'https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8' }}
+    //   target="_blank"
+    // >
+    <a href="https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8" target="_blank">
+      <button className={styles.btn}>12기 지원하기 </button>
+    </a>
   );
 }
 
