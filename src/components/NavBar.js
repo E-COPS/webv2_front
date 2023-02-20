8d2c046fa095f9c4ff0186b586657e4d09875fe4
mj <mjk4534@ewhain.net>
2023-02-21 01:35:38 +0900

    [Fix] NavBar 수정 반영

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/logo-img.png';
-import logo_hover from '../images/logo-green-img.png';
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-
 import { FiMenu } from 'react-icons/fi';
 import styles from './NavBar.module.css';
 
-function NavBar({ isWhite }) {
+function NavBar({ isWhite, isCurrent }) {
   return (
     <Navbar
       collapseOnSelect
@@ -18,12 +14,10 @@ function NavBar({ isWhite }) {
       className={styles.navStyle}
       style={{ backgroundColor: isWhite && '#E5E5E5' }}
     >
-      <Container className="align-items-center flex">
+      <Container className="px-0 align-items-center">
         {/* logo */}
-        <Navbar.Brand href="/" className="">
+        <Navbar.Brand href="/">
           <img src={logo} className={styles.logoImg} alt="e-cops" />
-          {/* Hover effect */}
-          {/* <img src={logo_hover} class="hover-logo" className={styles.logoImg} alt="ecops-hover" /> */}
         </Navbar.Brand>
         {/* menu icon */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="border-0 mx-2">
@@ -32,26 +26,42 @@ function NavBar({ isWhite }) {
         {/* menu */}
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav className="ml-auto align-items-center">
-            <Nav.Link className={styles.navText} href="/">
-              HOME
-            </Nav.Link>
-            {/* <Nav.Link
-              className={styles.navText}
-              // style={({ isActive }) => (isActive ? styles.activeStyle : {})}
-              href="/program"
-            >
-              PROGRAM
-            </Nav.Link>
-            <Nav.Link className={styles.navText} href="/member">
-              MEMBER
-            </Nav.Link>
-            <Nav.Link className={styles.navText} href="/activity">
-              ACTIVITY
-            </Nav.Link> */}
-
-            <Nav.Link className={styles.navText} href="/recruitment">
-              RECRUITMENT
-            </Nav.Link>
+            {isCurrent == 'program' ? (
+              <Nav.Link className={`${styles.navText} ${styles.navText__current}`} href="/program">
+                PROGRAM
+              </Nav.Link>
+            ) : (
+              <Nav.Link className={styles.navText} href="/program">
+                PROGRAM
+              </Nav.Link>
+            )}
+            {isCurrent == 'member' ? (
+              <Nav.Link className={`${styles.navText} ${styles.navText__current}`} href="/member">
+                MEMBER
+              </Nav.Link>
+            ) : (
+              <Nav.Link className={styles.navText} href="/member">
+                MEMBER
+              </Nav.Link>
+            )}
+            {isCurrent == 'activity' ? (
+              <Nav.Link className={`${styles.navText} ${styles.navText__current}`} href="/activity">
+                ACTIVITY
+              </Nav.Link>
+            ) : (
+              <Nav.Link className={styles.navText} href="/activity">
+                ACTIVITY
+              </Nav.Link>
+            )}
+            {isCurrent == 'recruit' ? (
+              <Nav.Link className={`${styles.navText} ${styles.navText__current}`} href="/recruitment">
+                RECRUITMENT
+              </Nav.Link>
+            ) : (
+              <Nav.Link className={styles.navText} href="/recruitment">
+                RECRUITMENT
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
