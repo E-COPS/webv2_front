b38c05e1a2d554a7213c9a52d3a68507618450d0
mj <mjk4534@ewhain.net>
2023-02-21 05:45:36 +0900

    rEVErt "[Fix] Member 수정"
    This reverts commit 3d793ca47b46eb9d5bbc9f217a22c12653dc314f.

--- src/components/Recruitment_Button.js
+++ src/components/Recruitment_Button.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 
 function Button({ text }) {
   return (
-    <button className={styles.btn}>
-      <a href="https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8">{text}</a>
-      {/* <Link to={{pathname: 'https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8'}} target="_blank">{text}</Link> */}
-    </button>
+    // <Link
+    //   to={{ pathname: 'https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8' }}
+    //   target="_blank"
+    // >
+    <a href="https://ewhaecops.notion.site/E-COPS-12th-Recruiting-81f6ece018cd49b68eaa1997996213a8" target="_blank">
+      <button className={styles.btn}>12기 지원하기 </button>
+    </a>
   );
 }
+
 //prop-types
 Button.propTypes = {
   text: PropTypes.string.isRequired,
